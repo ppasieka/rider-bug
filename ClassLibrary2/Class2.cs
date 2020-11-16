959a1fa003ac834221c512215f31d756d5ee0e42
Piotr Pasieka <ppasieka@gmail.com>
2020-11-16 18:17:16 +0100

    Project

---
+++ ClassLibrary2/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public void DoSomething()
+        {
+            var class1 = new ClassLibrary1.Class1();    
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary2;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var class2 = new Class2();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
